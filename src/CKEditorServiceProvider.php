6daabf0e2c18dc75d2c83c05146d83ecdfe99107
Vadim Shevelinski <shevelinski@gmail.com>
2025-05-22 14:28:14 +0300

    fix namespaces

--- src/CKEditorServiceProvider.php
+++ src/CKEditorServiceProvider.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Nakipelo\Orchid\CKEditor;
+namespace VadimShevelinski\OrchidCKEditor;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\View\Factory as ViewFactory;
@@ -12,34 +12,33 @@ class CKEditorServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->callAfterResolving('view', static function (ViewFactory  $factory) {
+        $this->callAfterResolving('view', static function (ViewFactory $factory) {
             $factory->composer('platform::app', static function () {
-                Dashboard::registerResource('scripts', asset('vendor/nakipelo/orchid-ckeditor/orchid_ckeditor.js'));
+                Dashboard::registerResource('scripts', asset('vendor/vadimshevelinski/orchid-ckeditor-full/orchid_ckeditor.js'));
             });
         });
 
         $this->offerPublishing();
     }
 
-    public function register()
+    public function register(): void
     {
-        $this->loadViewsFrom(__DIR__.'/../views', 'ckeditor');
-
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'ckeditor');
+        $this->loadViewsFrom(__DIR__ . '/../views', 'ckeditor');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'ckeditor');
     }
 
-    protected function offerPublishing()
+    protected function offerPublishing(): void
     {
-        if(! $this->app->runningInConsole()) {
+        if (! $this->app->runningInConsole()) {
             return;
         }
 
-		$this->publishes([
-			__DIR__.'/../public' => public_path('vendor/nakipelo/orchid-ckeditor'),
-		], ['ckeditor-assets', 'laravel-assets', 'orchid-assets']);
+        $this->publishes([
+            __DIR__ . '/../public' => public_path('vendor/vadimshevelinski/orchid-ckeditor-full'),
+        ], ['ckeditor-assets', 'laravel-assets', 'orchid-assets']);
 
         $this->publishes([
-            __DIR__.'/../config/config.php' => config_path('ckeditor.php'),
+            __DIR__ . '/../config/config.php' => config_path('ckeditor.php'),
         ], 'ckeditor-config');
     }
 }
